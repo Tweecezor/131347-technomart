b72f7ef5e87b7c79b6deb0cdd9aa66920ac3dc9b
Tweecezor <44469053+Tweecezor@users.noreply.github.com>
2018-12-24 21:06:48 +0300

    Подготовка к предзащите

---
+++ js/index-script.js
@@ -0,0 +1,33 @@
+
+	var popup = document.querySelector(".modal-write-us");
+	var locate = document.querySelector(".write-us");
+	var close = popup.querySelector(".modal-close");
+	locate.addEventListener("click",function(evt){
+		evt.preventDefault();
+		popup.classList.add("modal-show");
+	});
+	close.addEventListener("click",function(evt){
+		evt.preventDefault();
+		popup.classList.remove("modal-show");
+	});
+	 var mapLink = document.querySelector(".contacts-button-map");
+ 	 var mapPopup = document.querySelector(".modal-map");
+ 	 var mapClose = mapPopup.querySelector(".modal-close");
+ 	 mapLink.addEventListener("click", function (evt) {
+     evt.preventDefault();
+     mapPopup.classList.add("modal-show");
+     });
+
+    mapClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    mapPopup.classList.remove("modal-show");
+    });
+
+   window.addEventListener("keydown", function (evt) {
+    evt.preventDefault();
+    if (evt.keyCode === 27) {
+      if (mapPopup.classList.contains("modal-show")) {
+        mapPopup.classList.remove("modal-show");
+      }
+    }
+    });
